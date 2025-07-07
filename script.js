0ea5b24fadf8ea4c813cc0156286860333c881bd
lauryn <laurynmwase06@gmail.com>
2025-07-07 15:57:59 +0200

    added functionalities to password and confirm password

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const createAccount=document.querySelector(".create");
+const Password=document.querySelector("#password");
+const confirmPassword=document.querySelector("#conf_password");
+const text=document.querySelector(".text");
 
 createAccount.addEventListener("click",()=>{
     document.location="admin.html";
-})
+});
+if(password===confirmPassword){
+    text.textContent=" ";
+}
+else if(confirmPassword===" "){
+text.textContent=" Confirm your password ";
+}
+else if(password===""){
+    text.textContent=" Enter a password!!";
+}else if(Password!==confirmPassword && confirmPassword){
+    text.textContent="Passwords are not matching";
+}
+else{
+    text.textContent=" ";
+}
