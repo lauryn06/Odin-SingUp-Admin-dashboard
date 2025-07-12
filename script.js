659b04b7066f838a06534ac28301af2f1abc17cc
lauryn <laurynmwase06@gmail.com>
2025-07-12 04:21:28 +0200

    made changes to all the files

--- script.js
+++ script.js
@@ -1,49 +1,37 @@
-const createAccount=document.querySelector(".create");
-const Password=document.querySelector("#password");
-const confirmPassword=document.querySelector("#conf_password");
-
-const text=document.querySelector(".text");
-
-
+// const createAccount=document.querySelector(".create");
+// const Password=document.querySelector("#password");
+// const confirmPassword=document.querySelector("#conf_password");
+// const text=document.querySelector(".text");
+// const logout= document.querySelector(".logout");
 
 
-const login=document.querySelector("#login1");
-login.addEventListener("click",()=>{
-    document.location="login.html";
-});
+// const log=document.querySelector("#login");
 
-const logbtn=document.querySelector("#log");
-logbtn.addEventListener("click",(e)=>{
-    e.preventDefault();
-    document.location="admin.html";
-});
+// log.addEventListener("click",() =>{
 
-const logout=document.querySelector(".lg");
-logout.addEventListener("click",()=>{
-    document.location="index.html";
-});
+//     document.location="admin.html";
+// });
 
+// createAccount.addEventListener("click",()=>{
 
-createAccount.addEventListener("click",()=>{
+// if(password.value===confirmPassword.value){
+//     text.textContent=" Password Matched!";
+//         text.style.color="green";
+//           document.location="admin.html";
+// }
+// else if(confirmPassword.value===" "){
+// text.textContent=" Confirm your password ";
+// text.style.color="Red";
+// }
+// else if(password.value===""){
+//     text.textContent=" Enter a password!!";
+//     text.style.color="Red";
+// }else if(Password.value!==confirmPassword.value && confirmPassword){
+//     text.textContent="Passwords are not matching";
+//     text.style.color="Red";
+// }
 
-if(password.value===confirmPassword.value){
-    text.textContent=" Password Matched!";
-        text.style.color="green";
-          document.location="admin.html";
-}
-else if(confirmPassword.value===" "){
-text.textContent=" Confirm your password ";
-text.style.color="Red";
-}
-else if(password.value===""){
-    text.textContent=" Enter a password!!";
-    text.style.color="Red";
-}else if(Password.value!==confirmPassword.value && confirmPassword){
-    text.textContent="Passwords are not matching";
-    text.style.color="Red";
-}
-
-else{
-    text.textContent=" ";
-}
-});
+// else{
+//     text.textContent=" ";
+// }
+// });
