c05a94a2e89c99ee71fec2a66b56ee0bd72c141c
lauryn <laurynmwase06@gmail.com>
2025-07-13 05:34:16 +0200

    added logout option on dashboard

--- script.js
+++ script.js
@@ -1,37 +1,5 @@
-// const createAccount=document.querySelector(".create");
-// const Password=document.querySelector("#password");
-// const confirmPassword=document.querySelector("#conf_password");
-// const text=document.querySelector(".text");
-// const logout= document.querySelector(".logout");
+const userName= document.querySelector("'user");
 
+const Fname= document.querySelector(".First");
 
-// const log=document.querySelector("#login");
-
-// log.addEventListener("click",() =>{
-
-//     document.location="admin.html";
-// });
-
-// createAccount.addEventListener("click",()=>{
-
-// if(password.value===confirmPassword.value){
-//     text.textContent=" Password Matched!";
-//         text.style.color="green";
-//           document.location="admin.html";
-// }
-// else if(confirmPassword.value===" "){
-// text.textContent=" Confirm your password ";
-// text.style.color="Red";
-// }
-// else if(password.value===""){
-//     text.textContent=" Enter a password!!";
-//     text.style.color="Red";
-// }else if(Password.value!==confirmPassword.value && confirmPassword){
-//     text.textContent="Passwords are not matching";
-//     text.style.color="Red";
-// }
-
-// else{
-//     text.textContent=" ";
-// }
-// });
+userName.textContent=Fname;
